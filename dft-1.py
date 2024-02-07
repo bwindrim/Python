079b3e2c8e94d5c31ccde1d48eeade250343a078
Brian Windrim <bwindrim@gmail.com>
2024-02-07 08:29:18 +0000

    now using math.fsum(), and more testing of results

--- dft-1.py
+++ dft-1.py
@@ -1,4 +1,4 @@
-from math import pi, sin, cos
+from math import pi, sin, cos, fsum
 
 samples = 32
 fft_size = (samples >> 1) + 1
@@ -52,7 +52,7 @@ def idft2(rex, imx):
     REX[-1] = REX[-1]/2
     
     for i in range(N):
-        XX[i] = sum([REX[k]*cos(2*pi*k*i/N) +
+        XX[i] = fsum([REX[k]*cos(2*pi*k*i/N) +
                      IMX[k]*sin(2*pi*k*i/N) for k in range(len(REX))])
     
     return XX
@@ -74,7 +74,7 @@ def idft3(rex, imx):
     REX[0] = REX[0]/2
     REX[-1] = REX[-1]/2
     
-    XX = [sum([REX[k]*cos(2*pi*k*i/N) +
+    XX = [fsum([REX[k]*cos(2*pi*k*i/N) +
                IMX[k]*sin(2*pi*k*i/N)
                for k in range(len(REX))])
           for i in range(N)]
@@ -82,6 +82,8 @@ def idft3(rex, imx):
     return XX
 
 # Using a generator nested within a list comprehension
+# - possibly more efficient as fsum() takes any iterable as its
+# first argument rather than just a list.
 def idft4(rex, imx):
     "The Inverse Discrete Fourier Transform"
     assert len(rex) == fft_size
@@ -98,7 +100,7 @@ def idft4(rex, imx):
     REX[0] = REX[0]/2
     REX[-1] = REX[-1]/2
     
-    XX = [sum(REX[k]*cos(2*pi*k*i/N) +
+    XX = [fsum(REX[k]*cos(2*pi*k*i/N) +
                IMX[k]*sin(2*pi*k*i/N)
                for k in range(len(REX)))
           for i in range(N)]
@@ -133,8 +135,8 @@ def dft2(XX):
     N = len(XX) # N is the number of points in XX
         
     for k in range(len(REX)):
-        REX[k] =  sum([XX[i]*cos(2*pi*k*i/N) for i in range(N)])
-        IMX[k] = -sum([XX[i]*sin(2*pi*k*i/N) for i in range(N)])
+        REX[k] =  fsum([XX[i]*cos(2*pi*k*i/N) for i in range(N)])
+        IMX[k] = -fsum([XX[i]*sin(2*pi*k*i/N) for i in range(N)])
     
     return REX, IMX
 
@@ -144,22 +146,24 @@ def dft3(XX):
     assert len(XX) == samples
     N = len(XX) # N is the number of points in XX
     K = fft_size
-    REX = [sum([+XX[i]*cos(2*pi*k*i/N) for i in range(N)])
+    REX = [fsum([+XX[i]*cos(2*pi*k*i/N) for i in range(N)])
            for k in range(K)]
-    IMX = [sum([-XX[i]*sin(2*pi*k*i/N) for i in range(N)])
+    IMX = [fsum([-XX[i]*sin(2*pi*k*i/N) for i in range(N)])
            for k in range(K)]
     
     return REX, IMX
 
 # Using generators nested within list comprehensions
+# - possibly more efficient as fsum() takes any iterable as its
+# first argument rather than just a list.
 def dft4(XX):
     "The Discrete Fourier Transform"
     assert len(XX) == samples
     N = len(XX) # N is the number of points in XX
     K = fft_size
-    REX = [sum(+XX[i]*cos(2*pi*k*i/N) for i in range(N))
+    REX = [fsum(+XX[i]*cos(2*pi*k*i/N) for i in range(N))
            for k in range(K)]
-    IMX = [sum(-XX[i]*sin(2*pi*k*i/N) for i in range(N))
+    IMX = [fsum(-XX[i]*sin(2*pi*k*i/N) for i in range(N))
            for k in range(K)]
     
     return REX, IMX
@@ -176,19 +180,31 @@ rex1, imx1 = dft1(xx1)
 # assert imx1 == imx
 
 xx2 = idft2(rex, imx)
-assert xx2 == xx1
+#assert xx2 == xx1
 rex2, imx2 = dft2(xx2)
-assert rex2 == rex1
-assert imx2 == imx1
+# assert rex2 == rex1
+# assert imx2 == imx1
 
 xx3 = idft3(rex, imx)
 assert xx3 == xx2
 rex3, imx3 = dft3(xx3)
-assert rex2 == rex1
-assert imx2 == imx1
+assert rex3 == rex2
+assert imx3 == imx2
 
 xx4 = idft4(rex, imx)
 assert xx4 == xx3
 rex4, imx4 = dft4(xx4)
-assert rex2 == rex1
-assert imx2 == imx1
+assert rex4 == rex3
+assert imx4 == imx3
+
+yy = [1.0]*samples
+yy[0] = 1.0
+rex2, imx2 = dft2(yy)
+rex3, imx3 = dft3(yy)
+rex4, imx4 = dft4(yy)
+assert rex3 == rex2
+assert imx3 == imx2
+assert rex4 == rex3
+assert imx4 == imx3
+
+
