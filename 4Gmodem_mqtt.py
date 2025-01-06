95cc47c2a8af691d751a70f083bd1b30dd76034a
Brian Windrim <bwindrim@gmail.com>
2025-01-06 21:25:39 +0000

    Mostly comment changes

--- 4Gmodem_mqtt.py
+++ 4Gmodem_mqtt.py
@@ -64,30 +64,28 @@ with serial.Serial(port='/dev/ttyAMA0', baudrate=115200, timeout=1) as modem:
 
     # Start MQTT session
     print("Starting MQTT...")
-    print(send_at_command(f'CMQTTSTART'))  # Start MQTT service
-    time.sleep(2)  # Allow the service to initialize
-    print(send_at_command(f'CMQTTACCQ={client_index},"BWtestClient0",{int(use_ssl)}'))  # Set MQTT client ID, enable SSL by setting third parameter to 1
+    print(send_at_command(f'CMQTTSTART'))
+    time.sleep(2)
+    print(send_at_command(f'CMQTTACCQ={client_index},"BWtestClient0",{int(use_ssl)}'))
     print(send_at_command(f'CMQTTSSLCFG={client_index},{ssl_context}'))
 
     # Connect to MQTT broker
     print("Connecting to MQTT broker...")
     print(send_at_command(f'CMQTTCONNECT={client_index},"tcp://broker.hivemq.com:8883",{timeout},{int(clean_session)}'))
     #print(send_at_command(f'CMQTTCONNECT={client_index},"tcp://8d5ec6984ed54a29ac7794546055635d.s1.eu.hivemq.cloud:8883",{timeout},{int(clean_session)},"{username}","{password}"'))
-
     time.sleep(3)
 
     # Publish and be damned
     print(send_at_command(f'CMQTTTOPIC={client_index},{len(topic)}', payload=topic, timeout=1))  # Send topic
     print(send_at_command(f'CMQTTPAYLOAD={client_index},{len(message)}', payload=message, timeout=1))  # Send payload
     print(send_at_command(f'CMQTTPUB={client_index},{qos},{timeout},{int(retained)}'))  # Publish the message
-
     time.sleep(3)
 
     # Disconnect and stop MQTT
-    print(send_at_command(f'CMQTTDISC={client_index}')) # Disconnect from the broker
-    print(send_at_command(f'CMQTTREL={client_index}'))  # Disconnect from the broker
-    print(send_at_command(f'CMQTTSTOP'))   # Stop MQTT service
+    print(send_at_command(f'CMQTTDISC={client_index}')) # disconnect from the broker
+    print(send_at_command(f'CMQTTREL={client_index}'))  # release the client
+    print(send_at_command(f'CMQTTSTOP'))
 
-    # Deactivate PDP context (optional)
+    # Deactivate PDP context
     print("Deactivating PDP context...")
     print(send_at_command(f'CGACT=0,{pdp_context}'))
