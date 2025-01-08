80829d56ed058a4075f8aea04750251daefecc59
Brian Windrim <bwindrim@gmail.com>
2025-01-08 16:06:03 +0000

    Added set_last_will(), set_callback(), and subscribe(): - subscription notifications not handled yet

--- 4Gmodem_mqtt.py
+++ 4Gmodem_mqtt.py
@@ -56,11 +56,16 @@ class MQTTClient:
             self.port = 8883 if ssl else 1883
         else:
             self.port = port
+        self.cb = None
         self.user = user
         self.password = password
         self.keepalive = keepalive
         self.ssl_params = ssl_params
         self.client_index = 0
+        self.lw_topic = None
+        self.lw_msg = None
+        self.lw_qos = 0
+        self.lw_retain = False
         print(send_at_command(f'CMQTTACCQ={self.client_index},"{self.client_id}",{int(ssl)}'))
         if ssl:
             self.ssl_context = 1 # ToDo: just use client_index?
@@ -76,9 +81,8 @@ class MQTTClient:
             print(send_at_command(f'CSSLCFG="enableSNI",{self.ssl_context},{int(enable_SNI)}'))
 
             print(send_at_command(f'CMQTTSSLCFG={self.client_index},{self.ssl_context}'))
-        #print(send_at_command(f'CMQTTWILL={self.client_index},"BWtest/topic",1,1,"Goodbye, cruel world!"'))  # Set last will and testament
 
-    def connect(self, clean_session = True, timeout = 60):
+    def connect(self, clean_session = True, timeout = 60): # ToDO: default timeout should be 0
         if self.user:
             if self.password:
                 credentials = f',"{self.user}","{self.password}"'
@@ -87,6 +91,11 @@ class MQTTClient:
         else:
             credentials = ''
         self.timeout = timeout
+
+        if self.lw_topic:
+            print(send_at_command(f'CMQTTWILLTOPIC={self.client_index},{len(self.lw_topic)}', payload=self.lw_topic, timeout=self.timeout))  # Send topic
+            print(send_at_command(f'CMQTTWILLMSG={self.client_index},{len(self.lw_msg)},{self.lw_qos}', payload=self.lw_msg, timeout=self.timeout))  # Send payload
+
         print(send_at_command(f'CMQTTCONNECT={self.client_index},"tcp://{self.server_url}:{self.port}",{timeout},{int(clean_session)}{credentials}'))
         time.sleep(3)
         return False # ToDO: return true if connected to a persistent session?
@@ -97,6 +106,16 @@ class MQTTClient:
             print(send_at_command(f'CMQTTREL={self.client_index}'))  # release the client
             self.client_index = None
 
+    def set_last_will(self, topic, msg, retain=False, qos=0):
+        assert 0 <= qos <= 2
+        assert topic
+        self.lw_topic = topic
+        self.lw_msg = msg
+        self.lw_qos = qos
+        self.lw_retain = retain
+
+    def set_callback(self, f):
+        self.cb = f
 
     def publish(self, topic, msg, retain=False, qos=0):
         print(send_at_command(f'CMQTTTOPIC={self.client_index},{len(topic)}', payload=topic, timeout=self.timeout))  # Send topic
@@ -104,18 +123,22 @@ class MQTTClient:
         print(send_at_command(f'CMQTTPUB={self.client_index},{qos},{self.timeout},{int(retain)}'))  # Publish the message
         time.sleep(3)
 
+    def subscribe(self, topic, qos=0):
+        assert self.cb != None
+        print(send_at_command(f'CMQTTSUB={self.client_index},{len(topic)},{qos}', payload=topic, timeout=self.timeout))  # Subscribe to the topic
+
 with serial.Serial(port='/dev/ttyAMA0', baudrate=115200, timeout=1) as modem:
     # Start MQTT session
-    print("Starting MQTT...")
     MQTTStart("iot.1nce.net")
 
     # Verify IP address
-    print("Checking IP address...")
     print(send_at_command(f'CGPADDR=1'))
 
     ssl_params = {'ca_cert': 'isrgrootx1.pem', 'ssl_version': 3, 'auth_mode': 1, 'ignore_local_time': True, 'enable_SNI': True}
     client = MQTTClient("BWtestClient0", "8d5ec6984ed54a29ac7794546055635d.s1.eu.hivemq.cloud", port = 8883, user = "oisl_brian", password = "Oisl2023", ssl=True, ssl_params=ssl_params)
 
+    client.set_last_will(b"BWtest/lastwill", b"Goodbye, cruel world!", retain=True, qos=1)
+    
     # Connect to MQTT broker
     print("Connecting to MQTT broker...")
     client.connect()
