bff73d70ca7856069ef6f969c9cf8aa7aad9a2cd
Brian Windrim <bwindrim@gmail.com>
2025-06-11 23:19:28 +0100

    Added Cube-1.py:
    - works, after apt install freeglut3-dev

---
+++ Cube-1.py
@@ -0,0 +1,82 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+# Define the vertices and colors for the cube
+vertices = [
+    [1, 1, -1],
+    [1, -1, -1],
+    [-1, -1, -1],
+    [-1, 1, -1],
+    [1, 1, 1],
+    [1, -1, 1],
+    [-1, -1, 1],
+    [-1, 1, 1]
+]
+
+colors = [
+    [1, 0, 0],  # Red
+    [0, 1, 0],  # Green
+    [0, 0, 1],  # Blue
+    [1, 1, 0],  # Yellow
+    [1, 0, 1],  # Magenta
+    [0, 1, 1]   # Cyan
+]
+
+edges = [
+    [0, 1], [1, 2], [2, 3], [3, 0],
+    [4, 5], [5, 6], [6, 7], [7, 4],
+    [0, 4], [1, 5], [2, 6], [3, 7]
+]
+
+faces = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [0, 1, 5, 4],
+    [2, 3, 7, 6],
+    [0, 3, 7, 4],
+    [1, 2, 6, 5]
+]
+
+def draw_cube():
+    glBegin(GL_QUADS)
+    for i, face in enumerate(faces):
+        glColor3fv(colors[i % len(colors)])
+        for vertex in face:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+    glColor3f(0, 0, 0)
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    glTranslatef(0.0, 0.0, -5)
+    glRotatef(25, 1, 1, 0)
+    draw_cube()
+    glutSwapBuffers()
+
+def reshape(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45, width / height, 1, 50)
+    glMatrixMode(GL_MODELVIEW)
+
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800, 600)
+    glutCreateWindow(b"3D Cube with PyOpenGL")
+    glEnable(GL_DEPTH_TEST)
+    glutDisplayFunc(display)
+    glutReshapeFunc(reshape)
+    glutMainLoop()
+
+if __name__ == "__main__":
+    main()
